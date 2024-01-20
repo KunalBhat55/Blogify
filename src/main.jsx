d8be66f8a7243d67dfa684727d68e8051ac7735a
Kunal <kunalbhat6767@gmail.com>
2024-01-20 15:15:13 +0530

    app route

--- src/main.jsx
+++ src/main.jsx
@@ -63,7 +63,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
       <RouterProvider router={router}>
-        {/* <App /> */}
+        <App />
       </RouterProvider>
     </Provider>
   </React.StrictMode>
